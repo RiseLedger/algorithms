1b0d0001dd23c1195c6e96a569a08e2337046a69
Rise <riseledger@gmail.com>
2018-10-30 21:57:26 +0200

    Add depth-first search

---
+++ 20-depth-first-search/dfs.js
@@ -0,0 +1,30 @@
+const Stack = require('./../7-stack/Stack');
+
+function dfs(graph, start, end) {
+  const s = new Stack();
+  const visited = [];
+
+  s.push([start, []]);
+  while (!s.isEmpty()) {
+    const [vertex, path] = s.pop();
+
+    if (visited.includes(vertex)) {
+      continue;
+    }
+
+    visited.push(vertex);
+
+    if (vertex === end) {
+      return [...path, vertex];
+    }
+
+    const neighbours = graph[vertex];
+    for (let i = 0; i < neighbours.length; i++) {
+      s.push([neighbours[i], [...path, vertex]]);
+    }
+  }
+
+  return false;
+}
+
+module.exports = dfs;
