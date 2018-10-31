2e234e87dcd10026a99fc170d627254fdd176ab1
Rise <riseledger@gmail.com>
2018-10-31 22:13:53 +0200

    Handle cycle graph

--- 21-topological-sort/topologicalSort.js
+++ 21-topological-sort/topologicalSort.js
@@ -1,29 +1,30 @@
-const graph = require('./graph');
 const list = [];
+const tempMark = [];
 
 function topologicalSort(graph) {
   const nodes = Object.keys(graph);
 
   while (nodes.length) {
     const node = nodes.pop();
-    visit(node);
+    visit(node, graph);
   }
 
   return list.reverse();
 }
 
-function visit(node) {
+function visit(node, graph) {
   if (list.includes(node)) return;
+  if (tempMark.includes(node)) throw new Error('Graph has at least one cycle');
+
+  tempMark.push(node);
 
   const n = graph[node];
   for (let i = 0; i < n.length; i++) {
-    visit(n[i]);
+    visit(n[i], graph);
   }
 
+  tempMark.pop(node);
   list.push(node);
 }
-
-const topSort = topologicalSort(graph);
-console.log(topSort);
 
 module.exports = topologicalSort;
